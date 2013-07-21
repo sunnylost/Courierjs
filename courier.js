dcf616dbaf5016078adae6adf1d81b2dd7ef9f11
sunnylost <sunnylost@gmail.com>
2013-07-21 16:47:10 +0800

    0.2
    重构代码
    增加 before、after 方法
    修复 once 方法的错误行为

--- courier.js
+++ courier.js
@@ -7,54 +7,150 @@
         window[name] = definition;
     }
 })('Courier', function() {
+
+    function addEvent(handlers, name, around, fn, isOnce) {
+        if(!handlers[name]) {
+            handlers[name] = [];
+        }
+        if(typeof around == 'string') {
+            if(!handlers[name][around]) {
+                handlers[name][around] = [];
+            }
+            handlers[name][around].push({
+                fn: fn,
+                isOnce: isOnce
+            });
+        } else {
+            handlers[name].push({
+                fn: around,
+                isOnce: fn
+            });
+        }
+    }
+
+    function fireEvent(handlers, name, around, e) {
+        var hs = handlers[name],
+            hasAround = typeof around == 'string',
+            fn,
+            arr = [];
+
+        if(hasAround) {
+            hs = handlers[name][around];
+        } else {
+            e = around;
+        }
+
+        if(!hs) return;
+
+        while((fn = hs.shift())) {
+            if(!e.isStoped) {
+                fn.fn.call(null, e);
+                !fn.isOnce && arr.push(fn);
+            } else {
+                arr.push(fn);
+            }
+        }
+        arr.before = hs.before;
+        arr.after = hs.after;
+        hasAround ? (handlers[name][around] = arr) : (handlers[name] = arr);
+    }
+
+    function uniqueArray(arr, fn) {
+        var len = arr.length,
+            newArr = [],
+            item;
+        while(len--) {
+            item = arr.shift();
+            if(item !== fn) newArr.push(item);
+        }
+        newArr.before = arr.before;
+        newArr.after = arr.after;
+        arr = null;
+        return newArr;
+    }
+
     var C = {
-        handlers : {},
+        handlers: {},
 
-        on : function(name, handler) {
-            var h = this.handlers;
-            if(!h[name]) {
-                h[name] = [];
-            }
-            h[name].push(handler);
+        before: function(name, fn) {
+            this.on(name, fn, 'before');
+            return this;
         },
 
-        off : function(name) {
-            var hs = this.handlers[name];
-            if(hs) {
-                delete this.handlers[name];
-            }
+        after: function(name, fn) {
+            this.on(name, fn, 'after');
+            return this;
         },
 
-        once : function(name, handler) {
-            var h = this.handlers,
-                that = this;
-            if(!h[name]) {
-                h[name] = [];
-                h[name].push(function() {
-                    that.off(name);
-                });
+        on: function(name, fn, config) {
+            var isBefore = false,
+                isAfter = false,
+                isOnce = false,
+                that = this,
+                c;
+            if(config && typeof config == 'string') {
+                config = config.split(' ');
+                for(var i = 0, len = config.length; i < len; i++) {
+                    c = config[i];
+                    if(c.indexOf('before') >= 0) {
+                        isBefore = true;
+                    } else if(c.indexOf('after') >= 0) {
+                        isAfter = true;
+                    } else if(c.indexOf('once') >= 0) {
+                        isOnce = true;
+                    }
+                }
+                isBefore && addEvent(this.handlers, name, 'before', fn);
+                isAfter && addEvent(this.handlers, name, 'after', fn);
+                isOnce && addEvent(this.handlers, name, fn, isOnce);
+            } else {
+                addEvent(this.handlers, name, fn);
             }
-            h[name].push(handler);
+            return this;
         },
 
-        fire : function(name, data) {
+        off: function(name, fn, around) {
             var hs = this.handlers[name],
                 i = 0,
                 len,
-                isStop = false;
+                item,
+                arr;
+            if(hs) {
+                if(!fn && !around) {
+                    delete this.handlers[name];
+                } else if(fn && !around) {
+                    if(typeof fn == 'string') {
+                        delete hs[around];
+                    } else {
+                        this.handlers[name] = uniqueArray(hs, fn);
+                    }
+                } else {
+                    this.handlers[name][around] = uniqueArray(hs[around], fn);
+                }
+            }
+            return this;
+        },
 
-            if(!hs || (len = hs.length) == 0) return;
-            while(i < len && !isStop) {
-                hs[i++].call(null, {
+        once: function(name, fn) {
+            this.on(name, fn, 'once');
+            return this;
+        },
+
+        fire: function(name, data) {
+            var e = {
                     data: data,
                     /*
                         停止后续事件触发
                     */
                     stop: function() {
-                        isStop = true;
+                        this.isStoped = true;
                     }
-                });
-            }
+                };
+
+            fireEvent(this.handlers, name, 'before', e);
+            fireEvent(this.handlers, name, e);
+            fireEvent(this.handlers, name, 'after', e);
+            return this;
         }
     };
     return C;
